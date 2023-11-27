128853aac285648ae62ff15803109a0330bcea44
umang0310 <prajapatiumang2004@gmail.com>
2023-11-27 13:32:51 +0530

    first commit

---
+++ src/models/user.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema, mongo } from "mongoose";
+
+const UserSchema = new Schema({
+  name: String,
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Email Required !!"],
+  },
+  password: {
+    type: String,
+    require: [true, "Password Required !!"],
+  },
+  about: String,
+  profileURL: String,
+});
+
+export const User =
+  mongoose.models.users || mongoose.model("users", UserSchema);
